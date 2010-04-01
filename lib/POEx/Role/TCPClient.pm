c2dd0888f0a747dbf4a39eb21bf179bdb6838031
Nicholas Perez <nperez@cpan.org>
2010-04-01 07:00:05 +0000

    Import of NPEREZ/POEx-Role-TCPClient-1.100910 from CPAN.
    gitpan-cpan-distribution: POEx-Role-TCPClient
    gitpan-cpan-version:      1.100910
    gitpan-cpan-path:         NPEREZ/POEx-Role-TCPClient-1.100910.tar.gz
    gitpan-cpan-author:       NPEREZ
    gitpan-cpan-maturity:     released

--- lib/POEx/Role/TCPClient.pm
+++ lib/POEx/Role/TCPClient.pm
@@ -1,5 +1,5 @@
 package POEx::Role::TCPClient;
-our $VERSION = '0.100170';
+$POEx::Role::TCPClient::VERSION = '1.100910';
 
 #ABSTRACT: A Moose Role that provides TCPClient behavior
 
@@ -45,13 +45,13 @@ role POEx::Role::TCPClient
         lazy        => 1,
         default     => sub { {} },
         clearer     => 'clear_wheels',
-        handles    =>
+        handles     =>
         {
-            'get_wheel'    => 'get',
-            'set_wheel'    => 'set',
-            'delete_wheel' => 'delete',
-            'count_wheels' => 'count',
-            'has_wheel'    => 'exists',
+            get_wheel => 'get',
+            set_wheel => 'set',
+            delete_wheel => 'delete',
+            count_wheels => 'count',
+            has_wheel => 'exists',
         }
     );
 
@@ -80,11 +80,11 @@ role POEx::Role::TCPClient
         clearer     => 'clear_connection_tags',
         handles     =>
         {
-            'get_connection_tag'       => 'get',
-            'set_connection_tag'       => 'set',
-            'delete_connection_tag'    => 'delete',
-            'has_connection_tags'      => 'count',
-            'has_connection_tag'       => 'exists',
+            get_connection_tag => 'get',
+            set_connection_tag => 'set',
+            delete_connection_tag => 'delete',
+            has_connection_tags => 'count',
+            has_connection_tag => 'exists',
         }
 
     );
@@ -157,132 +157,7 @@ POEx::Role::TCPClient - A Moose Role that provides TCPClient behavior
 
 =head1 VERSION
 
-version 0.100170
-
-=head1 REQUIRES
-
-=head2 METHODS
-
-=head3 handle_inbound_data($data, WheelID $id) is Event
-
-This required method will be passed the data received, and from which wheel 
-it came. 
-
-=cut
-
-=pod
-
-=head1 ATTRIBUTES
-
-=head2 socket_factories traits: ['Hash'], isa: HashRef[Object]
-
-The POE::Wheel::SocketFactory objects created in connect are stored here and 
-managed via the following provides:
-
-    handles     =>
-    {
-        'get_socket_factory'       => 'get',
-        'set_socket_factory'       => 'set',
-        'delete_socket_factory'    => 'delete',
-        'has_socket_factories'     => 'count',
-        'has_socket_factory'       => 'exists',
-    }
-
-=cut
-
-=pod
-
-=head2 wheels traits: ['Hash'], isa: HashRef, clearer: clear_wheels
-
-When connections are finished, a POE::Wheel::ReadWrite object is created and 
-stored in this attribute, keyed by WheelID. Wheels may be accessed via the
-following provided methods. 
-
-    handles    =>
-    {
-        'get_wheel'    => 'get',
-        'set_wheel'    => 'set',
-        'delete_wheel' => 'delete',
-        'count_wheels' => 'count',
-        'has_wheel'    => 'exists',
-    }
-
-=cut
-
-=pod
-
-=head2 last_wheel is: rw, isa: WheelID
-
-This holds the last ID created from the handle_on_connect method. Handy if the
-protocol requires client initiation.
-
-=cut
-
-=pod
-
-=head2 filter is: rw, isa: Filter
-
-This stores the filter that is used when constructing wheels. It will be cloned
-for each connection completed.
-
-=cut
-
-=pod
-
-=head2 connection_tags metaclass: Collection::Hash, is: ro, isa: HashRef[Ref]
-
-This stores any arbitrary user data passed to connect keyed by the socket
-factory ID. Handy to match up multiple connects for composers.
-
-=cut
-
-=pod
-
-=head1 METHODS
-
-=head2 connect(Str :$remote_address, Int :$remote_port, Ref :$tag?) is Event
-
-connect is used to initiate a connection to a remote source. It accepts two 
-named arguments that both required, remote_address and remote_port. They are 
-passed directly to SocketFactory. If tag is provided, it will be stored in 
-connection_tags and keyed by the socket factory's ID.
-
-=cut
-
-=pod
-
-=head2 handle_on_connect(GlobRef $socket, Str $address, Int $port, WheelID $id) is Event
-
-handle_on_connect is the SuccessEvent of the SocketFactory instantiated in _start. 
-
-=cut
-
-=pod
-
-=head2 handle_connect_error(Str $action, Int $code, Str $message, WheelID $id) is Event
-
-handle_connect_error is the FailureEvent of the SocketFactory
-
-=cut
-
-=pod
-
-=head2 handle_socket_error(Str $action, Int $code, Str $message, WheelID $id) is Event
-
-handle_socket_error is the ErrorEvent of each POE::Wheel::ReadWrite instantiated.
-
-=cut
-
-=pod
-
-=head2 shutdown() is Event
-
-shutdown unequivically terminates the TCPClient by clearing all wheels and 
-aliases, forcing POE to garbage collect the session.
-
-=cut
-
-=pod
+version 1.100910
 
 =head1 DESCRIPTION
 
@@ -299,17 +174,143 @@ handle_inbound_data.
 
 The connect event must be invoked to initiate a connection.
 
+=head1 PROTECTED_ATTRIBUTES
+
+=head2 socket_factories
+
+    traits:Hash, isa: HashRef[Object]
+
+The POE::Wheel::SocketFactory objects created in connect are stored here and 
+managed via the following provides:
+
+    {
+        get_socket_factory => 'get',
+        set_socket_factory => 'set',
+        delete_socket_factory => 'delete',
+        has_socket_factories => 'count',
+        has_socket_factory => 'exists',
+    }
+
+=head2 wheels
+
+    traits: Hash, isa: HashRef, clearer: clear_wheels
+
+When connections are finished, a POE::Wheel::ReadWrite object is created and 
+stored in this attribute, keyed by WheelID. Wheels may be accessed via the
+following provided methods. 
+
+    {
+        get_socket_factory => 'get',
+        set_socket_factory => 'set',
+        delete_socket_factory => 'delete',
+        has_socket_factories => 'count',
+        has_socket_factory => 'exists',
+    }
+
+=head2 wheels 
+
+    traits: Hash, isa: HashRef, clearer: clear_wheels
+
+When connections are finished, a POE::Wheel::ReadWrite object is created and 
+stored in this attribute, keyed by WheelID. Wheels may be accessed via the
+following provided methods.
+
+    {
+        get_wheel => 'get',
+        set_wheel => 'set',
+        delete_wheel => 'delete',
+        count_wheels => 'count',
+        has_wheel => 'exists',
+    }
+
+=head2 last_wheel
+
+    is: rw, isa: WheelID
+
+This holds the last ID created from the handle_on_connect method. Handy if the
+protocol requires client initiation.
+
+=head2 filter
+
+    is: rw, isa: Filter
+
+This stores the filter that is used when constructing wheels. It will be cloned
+for each connection completed. By default, instantiates a POE::Filter::Line object.
+
+=head2 connection_tags
+
+    traits: Hash, is: ro, isa: HashRef[Ref]
+
+This stores any arbitrary user data passed to connect keyed by the socket
+factory ID. Handy to match up multiple connects for composers.
+
+    {
+        get_connection_tag => 'get',
+        set_connection_tag => 'set',
+        delete_connection_tag => 'delete',
+        has_connection_tags => 'count',
+        has_connection_tag => 'exists',
+    }
+
+=head1 PUBLIC_METHODS
+
+=head2 connect
+
+    (Str :$remote_address, Int :$remote_port, Maybe[Ref] :$tag) is Event
+
+connect is used to initiate a connection to a remote source. It accepts two 
+named arguments that both required, remote_address and remote_port. They are 
+passed directly to SocketFactory. If tag is provided, it will be stored in 
+connection_tags and keyed by the socket factory's ID.
+
+=head2 shutdown()
+
+    is Event
+
+shutdown unequivically terminates the TCPClient by clearing all wheels and 
+aliases, forcing POE to garbage collect the session.
+
+=head1 PROTECTED_METHODS
+
+=head2 handle_on_connect
+
+    (GlobRef $socket, Str $address, Int $port, WheelID $id) is Event
+
+handle_on_connect is the SuccessEvent of the SocketFactory instantiated in _start. 
+
+=head2 handle_connect_error
+
+    (Str $action, Int $code, Str $message, WheelID $id) is Event
+
+handle_connect_error is the FailureEvent of the SocketFactory
+
+=head2 handle_socket_error
+
+    (Str $action, Int $code, Str $message, WheelID $id) is Event
+
+handle_socket_error is the ErrorEvent of each POE::Wheel::ReadWrite instantiated.
+
+=head1 REQUIRES
+
+=head2 METHODS
+
+=head3 handle_inbound_data
+
+    ($data, WheelID $id) is Event
+
+This required method will be passed the data received, and from which wheel 
+it came. 
+
 =head1 AUTHOR
 
-Nicholas Perez <nperez@cpan.org>
+  Nicholas Perez <nperez@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is Copyright (c) 2010 by Nicholas Perez.
+This software is copyright (c) 2010 by Nicholas Perez.
 
-This is free software, licensed under:
-
-  The GNU General Public License, Version 3, June 2007
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
 
